0d02bf9a84fd2a35073e382c5b996348d3eacb56
akshit-jain01 <akshitjain.1112003@gmail.com>
2023-07-14 12:37:24 +0530

    server rectify

--- YantroMitra/settings.py
+++ YantroMitra/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'YantroMitra',
         'USER': 'postgres',
         'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
